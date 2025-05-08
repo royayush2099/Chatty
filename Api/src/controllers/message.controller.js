ac19a4d084758ce035af808aa8c7aa8756d9b3e3
Ayushroy <royayush2099@gmail.com>
2025-05-08 10:05:07 +0530

    added the backend

---
+++ Api/src/controllers/message.controller.js
@@ -0,0 +1,69 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js"
+import {v2 as cloudinary} from 'cloudinary';
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
+export const getUsersForSidebar = async (req,res)=>{
+ try{
+    const loggedInUserId =  req.user._id;//this route is protected so we can grab the useras it is passed 
+    const filterUsers = await User.find({_id:{$ne:loggedInUserId}}).select("-password");//this means find all users but not find the current logginuser 
+res.status(200).json(filterUsers); 
+}catch(err){
+console.error("Error in getUsersForSidebar:",err.message);
+res.status(500).json({error:"Internal server error"});
+}
+};
+
+export const getMessages = async (req,res)=>{
+    try{
+      const{id:userToChatId}= req.params;
+      const myId = req.user._id;//we can access this user._id because we passed a protectRoute middleware in message routes
+       //myId is the current user
+       //giving the condition below find all the meesages where i am a sender or i am a receiver
+      const messages = await Message.find({
+        $or:[
+            {senderId:myId, receiverId:userToChatId},
+            {senderId:userToChatId,receiverId:myId}
+        ]
+      })
+    
+      res.status(200).json(messages);
+    }catch(err){
+      console.log("Error in getMessages controller:",err.message);
+      res.status(500).json({error:"Internal server error"});
+    }
+}
+
+export const sendMessage = async (req,res)=>{
+    try{
+     const {text,image}= req.body;
+     const {id:receiverId} = req.params;
+     const senderId = req.user._id;
+     //checking if image is there
+     let imageUrl;
+     if(image){
+        //upload base64 image to cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(image);
+        imageUrl = uploadResponse.secure_url;
+     }
+     //message creation
+     const newMessage = new Message({
+        senderId,
+        receiverId,
+        text,
+        image:imageUrl,
+     })
+     await newMessage.save();
+     //todo:realtime functionality goes here =>socket.io
+const receiverSocketId = getReceiverSocketId(receiverId);
+if(receiverSocketId){
+  io.to(receiverSocketId).emit("newMessage",newMessage);
+}
+
+     res.status(201).json(newMessage)
+    }
+    catch(err){
+        console.log("Error in sendMessage controller",err.message);
+        res.status(500).json({err:"Internal server error"})
+    }
+}
---
+++ Api/src/lib/db.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose'
+
+export const connectDB = async ()=>{
+    try{
+       const conn =  await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDb connected : ${conn.connection.host}`)
+    }catch(err){
+        console.log("Mongodb connection error:",err)
+    }
+}
