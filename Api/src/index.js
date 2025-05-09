0a401d6b1b4054dc61fd8635db7f9bff8f96fed1
Ayushroy <royayush2099@gmail.com>
2025-05-09 10:36:06 +0530

    file transfer size increased

--- Api/src/index.js
+++ Api/src/index.js
@@ -12,7 +12,9 @@ import path from 'path'
 
 const PORT = process.env.PORT
 const __dirname = path.resolve();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+//increased the size of file transfer 
 app.use(cookieParser());
 app.use(cors({
     origin:'http://localhost:5173',
