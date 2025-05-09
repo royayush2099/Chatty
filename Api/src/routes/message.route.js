98473fa643c299ff7063aea2e63a38a7f605f40d
Ayushroy <royayush2099@gmail.com>
2025-05-09 09:21:13 +0530

    fixing routes error for production issue

--- Api/src/routes/message.route.js
+++ Api/src/routes/message.route.js
@@ -3,7 +3,7 @@ import { protectRoute } from '../middlewares/auth.middleware.js';
 import {getUsersForSidebar,getMessages,sendMessage} from '../controllers/message.controller.js'
 const router = express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar);
-router.get("/:id",protectRoute,getMessages)
-router.post("/send/:id",protectRoute,sendMessage)
+router.get("/users", protectRoute, getUsersForSidebar);
+router.post("/send/:id", protectRoute, sendMessage); // move this before ":id"
+router.get("/:id", protectRoute, getMessages);
 export default router;
